8d59a5db89b1077037ae831425f83fa63abe28c1
gorhkd <gorhkd223@gmail.com>
2024-12-17 16:25:13 +0900

    initial create

---
+++ src/test/java/com/apple/__12_17_shop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.__12_17_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
